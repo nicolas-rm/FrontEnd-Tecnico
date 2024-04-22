396a3b5a5c36784ca9590f5e44666fb0750edf6e
NICOLAS RM <nicolas.rm540@gmail.com>
2024-04-22 14:57:57 -0600

    Configuraciones basicaas del dashboard

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -25,22 +25,29 @@ function Dashboard() {
     };
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light position-relative">
+        <nav className="navbar navbar-expand-lg bg-light mt-0">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Mi Sitio</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <Link className="navbar-brand" to="/">Navbar</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/ruta1">Ruta 1</Link>
+                            <Link className="nav-link active" aria-current="page" to="/categorias">Categorias</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/ruta2">Ruta 2</Link>
+                            <Link className="nav-link" to="/link">Tematicas</Link>
                         </li>
-                        {/* Agrega más rutas aquí */}
+                        {/* Aquí puedes agregar tu propio componente Dropdown */}
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contenidos">Contenidos</Link>
+                        </li>
                     </ul>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
                 </div>
             </div>
         </nav>
